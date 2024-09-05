b4a00c865d3dcb98e1fb984112a1f2de585bd16e
jofrestrom <l.jofre@duocuc.cl>
2024-09-05 18:14:14 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Artemis-definitivo',
+  webDir: 'www'
+};
+
+export default config;
