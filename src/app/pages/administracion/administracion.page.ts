c371d8dd45a40b15d0f2be1b9aaa2255ca30e84c
DarvX01 <dreycoarak00@gmail.com>
2024-09-10 12:31:41 -0300

    pag de usuarios v 0.1 y reserva comenzando fix en vista celular y oprtimizar vista pc

--- src/app/pages/administracion/administracion.page.ts
+++ src/app/pages/administracion/administracion.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-administracion',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdministracionPage implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit() {
+  }
+
+
+  home(){
+    this.route.navigate(['/home']);
+  }
+  user(){
+    this.route.navigate(['/user']);
+  }
+  reserva(){
+    this.route.navigate(['/reserva']);
   }
 
 }
--- src/app/pages/user/user.page.ts
+++ src/app/pages/user/user.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserPage implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit() {
   }
+
+  home(){
+    this.route.navigate(['/home']);
+  }
+  admin(){
+    this.route.navigate(['/administracion']);
+  }
+  reserva(){
+    this.route.navigate(['/reserva']);
+  }
+
 
 }
